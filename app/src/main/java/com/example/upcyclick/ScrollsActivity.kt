05f52d0f0c633cfa8749d87a3709717c02841fa3
MAL1S <karbushev_ivan@mail.ru>
2021-07-31 13:19:59 +0800

    Added activities and noActionBar style

---
+++ app/src/main/java/com/example/upcyclick/ScrollsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.upcyclick
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrollsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolls)
+    }
+}
