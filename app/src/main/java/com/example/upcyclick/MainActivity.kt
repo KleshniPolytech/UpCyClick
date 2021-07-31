ab2580d31138d2a147a0d48855eed244832f6b92
MAL1S <karbushev_ivan@mail.ru>
2021-07-31 20:00:57 +0800

    Added bottom navigation and activities turned into fragments

--- app/src/main/java/com/example/upcyclick/MainActivity.kt
+++ app/src/main/java/com/example/upcyclick/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.upcyclick
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.room.Room
 import com.example.upcyclick.database.UpDB
 import com.example.upcyclick.database.entity.Scroll
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -16,5 +20,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottom_nav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = findNavController(R.id.fragment)
+
+        bottom_nav.setupWithNavController(navController)
     }
 }
