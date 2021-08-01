1628c52121322d3f48e6395b0e206421e2713155
MAL1S <karbushev_ivan@mail.ru>
2021-08-01 22:52:20 +0800

    Game fragment is done

--- app/src/main/java/com/example/upcyclick/quiz/QuizGameFragment.kt
+++ app/src/main/java/com/example/upcyclick/quiz/QuizGameFragment.kt
@@ -11,8 +11,33 @@ import com.example.upcyclick.R
 import com.example.upcyclick.database.entity.Question
 
 class QuizGameFragment : Fragment() {
+    data class Q(
+        val dif: Int,
+        val text: String,
+        val answers: List<String>)
 
-    lateinit var currentQuestion: Question
+    private val questions: MutableList<Q> = mutableListOf(
+        Q(dif = 1,
+            text = "2 + 2 = ?",
+            answers = listOf("1", "2", "3", "4")),
+        Q(dif = 1,
+            text = "2 + 5 = ?",
+            answers = listOf("4", "5", "6", "7")),
+        Q(dif = 2,
+            text = "5 * 3 = ?",
+            answers = listOf("15", "13", "14", "12")),
+        Q(dif = 2,
+            text = "7 * 9 = ?",
+            answers = listOf("54", "49", "63", "77")),
+        Q(dif = 3,
+            text = "lol?",
+            answers = listOf("lol", "lmao", "rofl", "shrek")),
+        Q(dif = 3,
+            text = "2 - 2 = ?",
+            answers = listOf("0", "1", "2", "3"))
+    )
+
+    lateinit var currentQuestion: Q
     lateinit var answers: MutableList<String>
     private val questionIndex = 0
     private val numQuestions = 2
