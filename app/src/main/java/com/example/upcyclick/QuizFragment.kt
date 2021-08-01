9bb1002bb84818c407a6d542640f7ce623fac9d5
MAL1S <karbushev_ivan@mail.ru>
2021-08-01 17:29:53 +0800

    Quiz difficulty selection fragment is ready

--- app/src/main/java/com/example/upcyclick/QuizFragment.kt
+++ app/src/main/java/com/example/upcyclick/QuizFragment.kt
@@ -5,13 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.Toast
+
 class QuizFragment : Fragment() {
+    lateinit var easyButton: RelativeLayout
+    lateinit var mediumButton: RelativeLayout
+    lateinit var hardButton: RelativeLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+        val view = inflater.inflate(R.layout.fragment_quiz, container, false)
+
+        init(view)
+        initListeners(view)
+
+        return view
+    }
+
+    private fun init(v: View) {
+        easyButton = v.findViewById(R.id.easyQuizButton)
+        mediumButton = v.findViewById(R.id.mediumQuizButton)
+        hardButton = v.findViewById(R.id.hardQuizButton)
+    }
+
+    private fun initListeners(v: View) {
+        easyButton.setOnClickListener {
+            Toast.makeText(context, "easy button", Toast.LENGTH_SHORT).show()
+        }
+        mediumButton.setOnClickListener {
+            Toast.makeText(context, "medium button", Toast.LENGTH_SHORT).show()
+        }
+        hardButton.setOnClickListener {
+            Toast.makeText(context, "hard button", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
