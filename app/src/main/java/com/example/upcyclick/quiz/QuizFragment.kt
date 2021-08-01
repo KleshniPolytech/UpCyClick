cdf8d61b7a088c60bca167bec3740d4ad7504e61
MAL1S <karbushev_ivan@mail.ru>
2021-08-02 01:54:22 +0800

    Quiz works without DB and difficult is hardcoded

--- app/src/main/java/com/example/upcyclick/quiz/QuizFragment.kt
+++ app/src/main/java/com/example/upcyclick/quiz/QuizFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RelativeLayout
-import android.widget.Toast
 import androidx.navigation.findNavController
 import com.example.upcyclick.R
 
