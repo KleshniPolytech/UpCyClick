612a8e917d5b2311dc91b27ea42ad647a56ffaae
luxorylife <lukash.201@mail.ru>
2021-08-03 13:57:48 +0800

    connection fragment_page_2 with YourManager create table "upgrades"

--- app/src/main/java/com/example/upcyclick/pager/PageFragment.kt
+++ app/src/main/java/com/example/upcyclick/pager/PageFragment.kt
@@ -1,5 +1,6 @@
 package com.example.upcyclick.pager
 
+import android.annotation.SuppressLint
 import android.widget.TextView
 
 
@@ -9,12 +10,19 @@ import android.view.ViewGroup
 
 import android.view.LayoutInflater
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 
 import androidx.fragment.app.Fragment
 import com.example.upcyclick.R
+import com.example.upcyclick.ShopFragment
+import com.example.upcyclick.YourManager
 
 
 class PageFragment : Fragment() {
+    val commonPrice: Int = 100
+    val rarePrice: Int = 1000
+    val legendaryPrice: Int = 10000
+
     private var mPage = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +35,43 @@ class PageFragment : Fragment() {
                      savedInstanceState: Bundle?): View? {
 
         val view = if (mPage == 1) inflater.inflate(R.layout.fragment_page, container, false) else inflater.inflate(R.layout.fragment_page_2, container, false)
+        val tvCoins2 = activity?.findViewById<TextView>(R.id.coins)
+
+
+        if (mPage == 1){
+
+        }
+        else{
+            val commonScroll = view.findViewById<ConstraintLayout>(R.id.lay_1)
+            commonScroll.setOnClickListener{
+                if (YourManager.getInstance(this.requireContext()).count >= commonPrice){
+                    YourManager.getInstance(this.requireContext()).count -= commonPrice
+                    tvCoins2?.text = YourManager.getInstance(this.requireContext()).count.toString() + " "
+                    //добавление данных о покупке рецепта в бд
+                    //открытие PDF с рецептом
+                }
+            }
+            val rareScroll = view.findViewById<ConstraintLayout>(R.id.lay_2)
+            rareScroll.setOnClickListener{
+                if (YourManager.getInstance(this.requireContext()).count >= rarePrice){
+                    YourManager.getInstance(this.requireContext()).count -= rarePrice
+                    tvCoins2?.text = YourManager.getInstance(this.requireContext()).count.toString() + " "
+                    //добавление данных о покупке рецепта в бд
+                    //открытие PDF с рецептом
+                }
+            }
+            val legendaryScroll = view.findViewById<ConstraintLayout>(R.id.lay_3)
+            legendaryScroll.setOnClickListener{
+                if (YourManager.getInstance(this.requireContext()).count >= legendaryPrice){
+                    YourManager.getInstance(this.requireContext()).count -= legendaryPrice
+                    tvCoins2?.text = YourManager.getInstance(this.requireContext()).count.toString() + " "
+                    //добавление данных о покупке рецепта в бд
+                    //открытие PDF с рецептом
+                }
+            }
+
+
+        }
 
         return view
     }
