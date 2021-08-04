43f36d43a908a6a09ff30dcc104593196df052a2
CoolSheff131 <a.nikulsheev@gmail.com>
2021-08-04 17:45:39 +0800

    added scroll button pattern

--- app/src/main/java/com/example/upcyclick/ScrollFragment.kt
+++ app/src/main/java/com/example/upcyclick/ScrollFragment.kt
@@ -10,6 +10,7 @@ class ScrollFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_scroll, container, false)
     }
 }
