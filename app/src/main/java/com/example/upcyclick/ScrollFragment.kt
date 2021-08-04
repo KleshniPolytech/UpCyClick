ee651ddb32ac81a359f6bf5a48aa106408f310fe
CoolSheff131 <a.nikulsheev@gmail.com>
2021-08-04 19:53:08 +0800

    добавлен адаптер для ресайклер вью скроллов, класс для связывания данных о скроллах и их представлением, макет скролла

--- app/src/main/java/com/example/upcyclick/ScrollFragment.kt
+++ app/src/main/java/com/example/upcyclick/ScrollFragment.kt
@@ -5,12 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ScrollFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_scroll, container, false)
 
-        return inflater.inflate(R.layout.fragment_scroll, container, false)
+        init(view)
+
+        recyclerView
+        return view
+    }
+    private fun init(v: View) {
+        recyclerView = v.findViewById(R.id.recyclerViewScrolls)
+        //todo передать контекст в метод
+        //recyclerView.layoutManager = LinearLayoutManager(контекст)
+        //todo передать список скроллов в конструктор
+        //recyclerView.adapter = CustomRecyclerAdapter( список)
+
     }
 }
